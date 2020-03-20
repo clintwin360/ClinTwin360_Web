e1a950288df0163969180bb7e75b23d167c9300e
Dylan Rumph <dprumph@gmail.com>
2020-03-20 15:36:21 -0600

    added references to glyphicons back in since the font can be added in later

--- clintwin/api/static/js/dynamicform.js
+++ clintwin/api/static/js/dynamicform.js
@@ -11,7 +11,8 @@ $(function()
         newEntry.find('input').val('');
         controlForm.find('.entry:not(:last) .btn-add')
             .removeClass('btn-add').addClass('btn-remove')
-            .removeClass('btn-success').addClass('btn-danger');
+            .removeClass('btn-success').addClass('btn-danger')
+            .html('<span class="glyphicon glyphicon-minus"></span>');
     }).on('click', '.btn-remove', function(e)
     {
 		$(this).parents('.entry:first').remove();
