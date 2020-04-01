6d96baa75e6ba8c958be9efd326e9b27a1e142b0
Dylan Rumph <dprumph@gmail.com>
2020-04-01 17:24:40 -0600

    updated trial dashboard, cards as radios not working

--- clintwin/sponsor/static/sponsor/js/viewtrials.js
+++ clintwin/sponsor/static/sponsor/js/viewtrials.js
@@ -2,12 +2,13 @@ var cardtitleTemplate = '<h5 class="card-title">';
 var cardtextTemplate = '<p class="card-text">';
 var cardtextsmallTemplate = '<p class="card-text"><small class="text-muted">'
 var cardTemplate = '<div class="card">';
-var cardbodyTemplate = '<div class="card-body">';
+var cardbodyTemplate = '<div class="card-body bg-light flex-row justify-content-between align-items-cent">';
+var radiocardTemplate = '<label class="mt-3"><input type="radio" name="trialselect" class="card-input-element d-none" id="test">';
 
 $(function(){
   $.getJSON("/sponsor/trial/?id=12345", function(result){
     $.each(result.results, function(i, field){
-      $("#cardlist").append(cardTemplate + cardbodyTemplate + cardtitleTemplate + field.title + '</h5>' + cardtextTemplate + field.description + "</p>" + cardtextsmallTemplate + 'Recruitment Start: ' + field.recruitmentStartDate + " Recruitment End: " + field.recruitmentEndDate + ' Enrollment Target: ' + field.enrollmentTarget + '</small></p></div></div>');
+      $("#cardlist").append(radiocardTemplate + cardTemplate + cardbodyTemplate + cardtitleTemplate + field.title + '</h5>' + cardtextTemplate + field.description + "</p>" + cardtextsmallTemplate + 'Recruitment Start: ' + field.recruitmentStartDate + "<br>Recruitment End: " + field.recruitmentEndDate + '<br>Enrollment Target: ' + field.enrollmentTarget + '</small></p></div></div></label<br>');
     });
   });
 });
