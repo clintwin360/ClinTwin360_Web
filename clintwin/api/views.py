3db385b55c367db9254a46d4f80b3d6c581d5b4e
Hemal Shah <heshah@constantcontact.com>
2020-04-02 19:55:00 -0400

    Reorg code and remove unwanted imports

--- clintwin/api/views.py
+++ clintwin/api/views.py
@@ -1,16 +1,8 @@
-##  Original additions
-from django.http import HttpResponse
-from requests import request, Response
-from rest_framework import permissions, status
+from rest_framework import permissions
 from django.contrib.auth import get_user_model
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.generics import CreateAPIView
 
-# Create your views here.
-from rest_framework.mixins import CreateModelMixin
-from rest_framework.viewsets import GenericViewSet
-
-from .models import UserSerializer
+from .serializers import UserSerializer
 
 
 class CreateUserView(CreateAPIView):
