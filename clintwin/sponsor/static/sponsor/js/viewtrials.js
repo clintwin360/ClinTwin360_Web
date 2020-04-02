fb10643d73314e3d691251a96b012453bd386a55
Dylan Rumph <dprumph@gmail.com>
2020-04-01 18:33:55 -0600

    further work on dashboard

--- clintwin/sponsor/static/sponsor/js/viewtrials.js
+++ clintwin/sponsor/static/sponsor/js/viewtrials.js
@@ -1,14 +1,15 @@
-var cardtitleTemplate = '<h5 class="card-title">';
+var radiocardTemplate = '<label class="mt-3"><input type="radio" name="trialselect" class="card-input-element d-none" id="test"/>';
+var cardTemplate = '<div class="card">';
+var cardheaderTemplate = '<div class="card-header"><h4 class="card-title">';
+var cardbodyTemplate = '<div class="card-body bg-light flex-row justify-content-between align-items-cent">';
+var cardtitleTemplate = '<h6 class="card-text">';
 var cardtextTemplate = '<p class="card-text">';
 var cardtextsmallTemplate = '<p class="card-text"><small class="text-muted">'
-var cardTemplate = '<div class="card">';
-var cardbodyTemplate = '<div class="card-body bg-light flex-row justify-content-between align-items-cent">';
-var radiocardTemplate = '<label class="mt-3"><input type="radio" name="trialselect" class="card-input-element d-none" id="test">';
 
 $(function(){
   $.getJSON("/sponsor/trial/?id=12345", function(result){
     $.each(result.results, function(i, field){
-      $("#cardlist").append(radiocardTemplate + cardTemplate + cardbodyTemplate + cardtitleTemplate + field.title + '</h5>' + cardtextTemplate + field.description + "</p>" + cardtextsmallTemplate + 'Recruitment Start: ' + field.recruitmentStartDate + "<br>Recruitment End: " + field.recruitmentEndDate + '<br>Enrollment Target: ' + field.enrollmentTarget + '</small></p></div></div></label<br>');
+      $("#cardlist").append(radiocardTemplate + cardTemplate + cardheaderTemplate + field.title + '</h4></div>' + cardbodyTemplate + cardtitleTemplate + field.objective + '</h6>' + cardtextTemplate + field.description + "</p>" + cardtextsmallTemplate + 'Recruitment Start: ' + field.recruitmentStartDate + "<br>Recruitment End: " + field.recruitmentEndDate + '<br>Enrollment Target: ' + field.enrollmentTarget + '</small></p></div></div></label<br>');
     });
   });
 });
