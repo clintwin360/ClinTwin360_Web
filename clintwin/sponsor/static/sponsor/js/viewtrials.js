a415ded02355fb544f3cf016c42f9ac4b271413f
Dylan Rumph <dprumph@gmail.com>
2020-04-02 02:15:04 -0600

    update to use new endpoint

--- clintwin/sponsor/static/sponsor/js/viewtrials.js
+++ clintwin/sponsor/static/sponsor/js/viewtrials.js
@@ -7,7 +7,7 @@ var cardtextTemplate = '<p class="card-text">';
 var cardtextsmallTemplate = '<p class="card-text"><small class="text-muted">'
 
 $(function(){
-  $.getJSON("/sponsor/trial/?id=12345", function(result){
+  $.getJSON("/sponsor/trials", function(result){
     $.each(result.results, function(i, field){
       $("#cardlist").append(radiocardTemplate + cardTemplate + cardheaderTemplate + field.title + '</h5><small>0/' + field.enrollmentTarget + '</small></div>' + cardbodyTemplate + cardtextTemplate + field.description + "</p>" + cardtextsmallTemplate + 'Recruitment Period: ' + field.recruitmentStartDate + " to " + field.recruitmentEndDate + '</small></p></div></div></label>');
     });
