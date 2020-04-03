55f66e3c280ed0eb476b798029ecad4ed6acc580
Ryan Gallagher <ryan_gallagher@apple.com>
2020-04-02 20:37:27 -0400

    question ranking work

--- clintwin/sponsor/serializers.py
+++ clintwin/sponsor/serializers.py
@@ -1,30 +1,36 @@
 from .models import *
 from rest_framework import serializers
 
+
 class ParticipantQuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = ParticipantQuestion
         fields = ['id', 'text', 'valueType', 'options']
+
 
 class ParticipantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Participant
         fields = ['id', 'first_name', 'last_name', 'email']
 
+
 class ParticipantResponseSerializer(serializers.ModelSerializer):
     class Meta:
         model = ParticipantResponse
         fields = ['question', 'participant', 'value']
+
 
 class SponsorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sponsor
         fields = '__all__'
 
+
 class SponsorDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sponsor
         fields = ['organization', 'contactPerson', 'email']
+
 
 class ClinicalTrialListSerializer(serializers.ModelSerializer):
     sponsor = SponsorDetailSerializer(read_only=True)
@@ -34,6 +40,7 @@ class ClinicalTrialListSerializer(serializers.ModelSerializer):
         fields = ['title', 'objective', 'trialId', 'sponsor', 'recruitmentStartDate',
                   'recruitmentEndDate', 'enrollmentTarget', 'description', 'url',]
 
+
 class ClinicalTrialDetailSerializer(serializers.ModelSerializer):
     sponsor = SponsorDetailSerializer(read_only=True)
 
@@ -41,6 +48,7 @@ class ClinicalTrialDetailSerializer(serializers.ModelSerializer):
         model = ClinicalTrial
         fields = ['title', 'objective', 'description', 'recruitmentStartDate',
                   'recruitmentEndDate', 'trialId', 'sponsor', 'url']
+
 
 class ClinicalTrialMatchSerializer(serializers.ModelSerializer):
     clinical_trial = ClinicalTrialListSerializer(read_only=True)
