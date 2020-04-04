61a765d1492001f3589eb2fbcfab65ad03c22b22
Hemal Shah <heshah@constantcontact.com>
2020-04-04 14:51:12 -0400

    Removing the password from settings.py file

--- clintwin/clintwin/settings.py
+++ clintwin/clintwin/settings.py
@@ -163,5 +163,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'clintwin360@gmail.com'
-EMAIL_HOST_PASSWORD = 'Clintwine599#'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
