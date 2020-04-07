dc65d3a8816a776c0ae795244ea1b2acfde6c36a
Dylan Rumph <dprumph@gmail.com>
2020-04-07 13:12:58 -0600

    update js

--- clintwin/sponsor/static/sponsor/js/viewsponsors.js
+++ clintwin/sponsor/static/sponsor/js/viewsponsors.js
@@ -1,11 +1,13 @@
-var tableEntryTemplate = '<tr onclick=""><td><a class="sponsor-id-link" href="">';
+var tableEntryTemplate = '<td><a class="sponsor-id-link" href="/api/trial/?id="';
+var tableRowTemplate = '<tr onclick="" id="';
 var editButton = '<td><button class="viewbtn" type="button">View</button><button class="editbtn" type="button">Edit</button></td>';
 
 $(function(){
   $.getJSON("/api/profile/", function(result){
     $.each(result.results, function(i, field){
-      $("#sponsor-list-body").append(tableEntryTemplate +
-        field.sponsor_id + '</a></td><td>' +
+      $("#sponsor-list-body").append(tableRowTemplate + field.id + '">' +
+        tableEntryTemplate + field.id + '">' +
+        field.id + '</a></td><td>' +
         field.organization + '</td><td>Date Joined: ' +
         field.date_joined + '</td><td>Notes:' +
         field.notes + '</td>' +
