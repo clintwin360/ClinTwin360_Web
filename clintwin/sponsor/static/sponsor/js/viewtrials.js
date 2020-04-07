61f0ee4e0faa17222b1e2936547bfc3a317f8edd
Dylan Rumph <dprumph@gmail.com>
2020-04-07 14:44:17 -0600

    basic dashboard js functionally should work now

--- clintwin/sponsor/static/sponsor/js/viewtrials.js
+++ clintwin/sponsor/static/sponsor/js/viewtrials.js
@@ -1,13 +1,13 @@
-var tableEntryTemplate = '<td><a class="sponsor-id-link" href="/api/trial/?id="';
-var tableRowTemplate = '<tr onclick="" id="';
+var tableEntryTemplate = '<td><a class="sponsor-id-link" href="/api/trial/?id=';
+var tableRowTemplate = '<tr onclick="" id=';
 var editButton = '<td><button class="viewbtn" type="button">View</button><button class="editbtn" type="button">Edit</button></td>';
 
 
 $(function(){
   $.getJSON("/api/trials/", function(result){
     $.each(result.results, function(i, field){
-      $("#trial-list-body").append(tableRowTemplate + field.id + '">' +
-        tableEntryTemplate + field.id + '">' +
+      $("#trial-list-body").append(tableRowTemplate + field.id.toString() + '>' +
+        tableEntryTemplate + field.id.toString() + '">' +
         field.custom_id + '</a></td><td>' +
         field.title + '</td><td>' +
         field.status + '</td><td>' +
