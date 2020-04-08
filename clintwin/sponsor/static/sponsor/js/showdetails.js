f92715397a4f5bd32a10ee7e895ac44e33852d99
Dylan Rumph <dprumph@gmail.com>
2020-04-08 12:15:51 -0600

    show detail fix

--- clintwin/sponsor/static/sponsor/js/showdetails.js
+++ clintwin/sponsor/static/sponsor/js/showdetails.js
@@ -12,3 +12,11 @@
         });
     });
 */
+
+$(document).on( "click","tr.clickable-row", function() {
+    var idArray = this.id.toString().split("_");
+    var trialId = idArray[1];
+    $.getJSON("/api/trial/?id=" + trialId, function(result){
+        $('#trial-details').append('<p>The trial selected has a pk of ' + trialId + '</p><br>');
+    });
+});
