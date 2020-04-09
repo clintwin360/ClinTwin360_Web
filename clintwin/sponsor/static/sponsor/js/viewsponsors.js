ca02fea836b3d080d55a229ded50cf10374b9cd3
Dylan Rumph <dprumph@gmail.com>
2020-04-09 16:12:13 -0600

    made contact email clickable

--- clintwin/sponsor/static/sponsor/js/viewsponsors.js
+++ clintwin/sponsor/static/sponsor/js/viewsponsors.js
@@ -3,7 +3,7 @@ function sponsor_row(props){
         "<tr id=\"sponsor-"+ props.id +"\" class=\"clickable-row sponsor-element\">" +
         "<td><a class=\"sponsor-id-link\" href=\"/sponsor/profile/"+ props.id + "\">" + props.id + "</a></td>" +
         "<td>"+props.organization+"</td>" +
-        "<td>"+props.contactPerson+" : "+props.email+"</td>" +
+        "<td>"+props.contactPerson+" : <a href=\"mailto:"+props.email+"\">"+props.email+"</a></td>" +
         "<td>"+props.date_joined+"</td>" +
         "<td>"+props.notes+"</td>" +
         "<td><a class=\"btn btn-info editbtn\" href=\"/sponsor/updateprofile/"+props.id+"\">Edit</td>" +
