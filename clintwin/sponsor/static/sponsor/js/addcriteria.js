b3cca4d73ad1549f1adca14ced9c9e28cfa4ebec
adaeze <adaeze.ezeh1@gmail.com>
2020-04-16 16:55:54 -0700

    updated criteria detail styling

--- clintwin/sponsor/static/sponsor/js/addcriteria.js
+++ clintwin/sponsor/static/sponsor/js/addcriteria.js
@@ -1,6 +1,12 @@
 // Need to grab th criteria entered and populate the "add criteria detail" page
-$(function () {
-    // Add Criteria button
-    $(".add_btn").modalForm({formURL: "{% url 'add_criteria' %}"});
+var popup1 = document.getElementById("popup-1")
+var openPopup1 = document.getElementById("add_btn")
+var closePopup1 = document.getElementById('back_cri')
 
-  });
+openPopup1.addEventListener('click', () => {
+	popup1.style.display = "block";
+})
+
+closePopup1.addEventListener('click', () => {
+	popup1.style.display = "none";
+})
