5d14bf3c92a2816855cf7bd46fbe0041b07a3c90
adaeze <adaeze.ezeh1@gmail.com>
2020-05-10 18:16:58 -0700

    Increased font size in forms and criteria page

---
+++ clintwin/clintwin/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/adaeze/clintwin360/ClinTwin360_Web/clintwin/clintwin/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
